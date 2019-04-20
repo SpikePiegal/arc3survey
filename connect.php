8d64788930449c148fa9985880124c8a7d78d041
SpikePiegal <david97mills@gmail.com>
2019-04-20 17:35:28 -0400

    Add prepared statements

--- connect.php
+++ connect.php
@@ -3,40 +3,14 @@
 $json = (file_get_contents("php://input"));
 
  $obj = json_decode($json,true);
- print_r($json);
- print_r($obj);
 
- function consoleLogs($data) {
-    $html = "";
-    $coll;
-    if (is_array($data) || is_object($data)) {
-        $coll = json_encode($data);
-    } else {
-        $coll = $data;
-    }
-    $html = "<script>console.log('PHP: ".$coll."');</script>";
-    echo($html);
-}
-
-consoleLogs(array("test1", "test2"));
-
-debug_to_console( "Test" ); 
-
-debug_to_console($obj);
- print_r("hello");
- echo "<script>console.log('" . json_encode($json) . "');</script>";
- echo "<script>console.log('" . $obj . "');</script>";
- echo "<script>console.log('" . '$obj' . "');</script>";
- var_dump('Hello');
- var_dump($obj);
- var_dump($json);
 header("Content-Type: application/json; charset=UTF-8")
 
 
 $servername = "localhost";
-$username = "username";
+$username = "root";
 $password = "password";
-$dbname = "myDB";
+$dbname = "arc";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -45,78 +19,61 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-/*
-$sql = INSERT INTO `arc`.`results`
-(`Result_ID`,
-`Age`,
-`Race_Ethnicity`,
-`International_Student`,
-`Sexual_Orientation`,
-`Year_Of_School`,
-`Participations`,
-`Living_Situation`,
-`Question1_SH`,
-`Question2_SH`,
-`Question3_SH`,
-`Question4_SH`,
-`Question5_SH`,
-`Question6_SH`,
-`Question7_SH`,
-`Question8_SH`,
-`Question9_SH`,
-`Question10_SH`,
-`Question11_SH`,
-`Question12_SH`,
-`Question13_SH`,
-`Question14_SH`,
-`Question15_SH`,
-`Question16_SH`,
-`Time_SH_1stQuestions`,
-`F_Up_Question1_SH`,
-`F_Up_Question2_SH`,
-`F_Up_Question3_SH`,
-`F_Up_Question4_SH`,
-`F_Up_Question5_SH`,
-`Student_ID`,
-`Survey_ID`,
-`Time_SH_2nQuestions`)
+$Age = $obj [0][1]; 
+$Race_Ethnicity
+$International_Student
+$Sexual_Orientation
+$Year_Of_School
+$Participations
+$Living_Situation
+$Question1_SH
+$Question2_SH
+$Question3_SH
+$Question4_SH
+$Question5_SH
+$Question6_SH
+$Question7_SH
+$Question8_SH
+$Question9_SH
+$Question10_SH
+$Question11_SH
+$Question12_SH
+$Question13_SH
+$Question14_SH
+$Question15_SH
+$Question16_SH
+$F_Up_Question1_SH
+$F_Up_Question2_SH
+$F_Up_Question3_SH
+$F_Up_Question4_SH
+$F_Up_Question5_SH
 
 
-VALUES
-(<{Result_ID: }>,
-<{Age: }>,
-<{Race_Ethnicity: }>,
-<{International_Student: }>,
-<{Sexual_Orientation: }>,
-<{Year_Of_School: }>,
-<{Participations: }>,
-<{Living_Situation: }>,
-<{Question1_SH: }>,
-<{Question2_SH: }>,
-<{Question3_SH: }>,
-<{Question4_SH: }>,
-<{Question5_SH: }>,
-<{Question6_SH: }>,
-<{Question7_SH: }>,
-<{Question8_SH: }>,
-<{Question9_SH: }>,
-<{Question10_SH: }>,
-<{Question11_SH: }>,
-<{Question12_SH: }>,
-<{Question13_SH: }>,
-<{Question14_SH: }>,
-<{Question15_SH: }>,
-<{Question16_SH: }>,
-<{Time_SH_1stQuestions: }>,
-<{F_Up_Question1_SH: }>,
-<{F_Up_Question2_SH: }>,
-<{F_Up_Question3_SH: }>,
-<{F_Up_Question4_SH: }>,
-<{F_Up_Question5_SH: }>,
-<{Student_ID: }>,
-<{Survey_ID: }>,
-<{Time_SH_2nQuestions: }>);
 
-*/
+$sql = "INSERT INTO results ( Age, Race_Ethnicity, International_Student, Sexual_Orientation,
+ Year_Of_School, Participations, Living_Situation, Question1_SH, Question2_SH, Question3_SH,
+ Question4_SH, Question5_SH, Question6_SH, Question7_SH, Question8_SH, Question9_SH,
+ Question10_SH, Question11_SH, Question12_SH, Question13_SH, Question14_SH, Question15_SH, Question16_SH,
+ F_Up_Question1_SH, F_Up_Question2_SH, F_Up_Question3_SH, F_Up_Question4_SH, F_Up_Question5_SH)
+ VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
+) ;"
+
+$stmt = mysqli_stmt_init($conn);
+
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+    echo "SQL Error";
+} else {
+    //bind paramaters to the placeholders
+    mysqli_stmt_bind_param($stmt, "ssssssssssssssssssssssssssss", $Age, $Race_Ethnicity, $International_Student, $Sexual_Orientation,
+    $Year_Of_School, $Participations, $Living_Situation, $Question1_SH, $Question2_SH, $Question3_SH,
+    $Question4_SH, $Question5_SH, $Question6_SH, $Question7_SH, $Question8_SH, $Question9_SH,
+    $Question10_SH, $Question11_SH, $Question12_SH, $Question13_SH, $Question14_SH, $Question15_SH, $Question16_SH,
+    $F_Up_Question1_SH, $F_Up_Question2_SH, $F_Up_Question3_SH, $F_Up_Question4_SH, $F_Up_Question5_SH
+     );
+     mysqli_stmt_execute($stmt);
+}
+
+
+
 
  ?>
