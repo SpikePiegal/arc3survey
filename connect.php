ca63d793ff1ef6795665cf0e20d4e1be094b4b7b
SpikePiegal <david97mills@gmail.com>
2019-04-22 19:47:22 -0400

    mysql

--- connect.php
+++ connect.php
@@ -15,22 +15,16 @@ $username = "root";
 $password = "password";
 $dbname = "arc";
 
-/* Create connection
+ Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-*/
+
 
 //
-$connection = mysql_connect($servername, $username, $password,$dbname);
-mysql_query("set names 'utf8'");
-if (!$connection) {
-    die("could not connect to the database.\n" . mysql_error());
-}else {
-    mysql_select_db(“$dbname”);
-}
+
 
 
 $Age = .$obj->q1.; 
